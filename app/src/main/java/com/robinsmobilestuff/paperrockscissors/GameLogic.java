6805991b094fa3b92ec32a258b413da50773aebb
Robin Lamb <56711156+robinlamb@users.noreply.github.com>
2020-02-18 11:28:56 -0600

    Add comments
    Added comments to better explain code and class purpose

--- app/src/main/java/com/robinsmobilestuff/paperrockscissors/GameLogic.java
+++ app/src/main/java/com/robinsmobilestuff/paperrockscissors/GameLogic.java
@@ -1,18 +1,20 @@
 package com.robinsmobilestuff.paperrockscissors;
 
-/**
- * Created by Robin on 9/29/2017.
- */
+// Class to figure the game logic given two choices of paper, rock, or scissors
 
 public class GameLogic {
+    
+    //Constants to represent possible choices
     public static final int PAPER = 10;
     public static final int ROCK = 20;
     public static final int SCISSORS = 30;
 
+    //Constants to represent possible outcome messages
     public static final int PLAYER_ONE_WINS = 1;
     public static final int PLAYER_TWO_WINS = 2;
     public static final int DRAW = 3;
 
+    //Constants to represent possible outcome pictures
     public static final int PAPER_COVERS_ROCK = 40;
     public static final int SCISSORS_CUT_PAPER = 50;
     public static final int ROCK_BREAKS_SCISSORS = 60;
@@ -20,15 +22,19 @@ public class GameLogic {
     public static final int TWO_SCISSORS = 80;
     public static final int TWO_ROCKS = 90;
 
-
+    //Message and picture to show game outcome
     public int Message;
     public int Picture;
 
     public GameLogic(int player1selection, int player2selection){
 
-
+        //Outer switch statement to consider first player's choice
         switch(player1selection) {
+      
             case PAPER:
+                
+                //Inner switch statements to consider second player's choice to figure the which
+                //message and picture will be displayed
                 switch (player2selection) {
                     case PAPER: {
                         Message = DRAW;
@@ -49,6 +55,7 @@ public class GameLogic {
                 break;
 
             case ROCK:
+                //Inner switch statement
                 switch (player2selection) {
                     case PAPER: {
                         Message = PLAYER_TWO_WINS;
@@ -70,8 +77,10 @@ public class GameLogic {
 
                 }//end switch
                 break;
+                
             default:
                 switch (player2selection) {
+                //Inner switch statement
                     case PAPER: {
                         Message = PLAYER_ONE_WINS;
                         Picture = SCISSORS_CUT_PAPER;
